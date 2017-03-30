3cf95dd4f8ac024e17a9aca40cb2e5d6dce90986
Mon <mz@codeia.ph>
2017-03-30 17:40:49 +0800

    nuked the deprecated types and the permission test
    confirmed the tests don't work in an emu. i swear they used to work before.
    gotta redo everything

--- vanilla-android-tests/src/main/java/ph/codeia/androidutilstests/PermissionsActivity.java
+++ vanilla-android-tests/src/main/java/ph/codeia/androidutilstests/PermissionsActivity.java
@@ -1,27 +1,24 @@
 package ph.codeia.androidutilstests;
 
 import android.Manifest;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AlertDialog;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
-import ph.codeia.androidutils.AndroidOldPermit;
-import ph.codeia.security.OldPermit;
-import ph.codeia.security.Sensitive;
+import ph.codeia.androidutils.AndroidPermit;
+import ph.codeia.meta.StrawMan;
+import ph.codeia.security.Permit;
 import ph.codeia.signal.Channel;
 import ph.codeia.signal.Links;
 import ph.codeia.signal.SimpleChannel;
 
+@StrawMan
 public class PermissionsActivity extends TestActivity {
 
     static final Channel<String> COARSE = new SimpleChannel<>();
@@ -29,27 +26,46 @@ public class PermissionsActivity extends TestActivity {
     static final Channel<String> CONTACTS = new SimpleChannel<>();
     static final Channel<String> EDGE_CASE = new SimpleChannel<>();
 
-    final List<Sensitive> requests = new ArrayList<>();
-    final AtomicInteger counter = new AtomicInteger(1);
+    private static final int ASK_COARSE = 1;
+    private static final int ASK_FINE = 2;
+    private static final int ASK_CONTACTS = 3;
+    private static final int ASK_ALL = 4;
+
     private Channel.Link links;
+    private Permit coarse, fine, contacts, combo;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         status = (TextView) findViewById(R.id.the_status);
-        Collections.addAll(requests,
-                ask(Manifest.permission.ACCESS_COARSE_LOCATION)
-                        .granted(() -> COARSE.send("granted")),
-                ask(Manifest.permission.ACCESS_FINE_LOCATION)
-                        .granted(() -> FINE.send("granted")),
-                ask(Manifest.permission.READ_CONTACTS)
-                        .granted(() -> CONTACTS.send("granted")),
-                ask(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.READ_CONTACTS)
-                        .granted(() -> {
-                            COARSE.send("granted");
-                            CONTACTS.send("granted");
-                        }));
+        AndroidPermit.Helper permits = AndroidPermit.of(getSupportFragmentManager());
+        permits.setBefore(appeal -> {
+            publish("appeal", appeal.permissions());
+            new AlertDialog.Builder(this)
+                    .setTitle("Please?")
+                    .setMessage(""
+                            + "I need these permissions to continue:\n-  "
+                            + TextUtils.join("\n-  ", appeal.permissions()))
+                    .setPositiveButton("Ask me again", (dialog, id) -> appeal.submit())
+                    .create()
+                    .show();
+        });
+        permits.setAfter(response -> {
+            publish("denied", response.forbidden());
+            if (!response.appeal()) {
+                tell("permanently denied:%n-  %s", TextUtils.join("\n-  ", response.forbidden()));
+            }
+        });
+        coarse = permits
+                .ask(ASK_COARSE, Manifest.permission.ACCESS_COARSE_LOCATION)
+                .granted(() -> COARSE.send("granted"));
+        fine = permits
+                .ask(Manifest.permission.ACCESS_FINE_LOCATION)
+                .granted(() -> FINE.send("granted"));
+        contacts = permits
+                .ask(ASK_CONTACTS, Manifest.permission.READ_CONTACTS)
+                .granted(() -> CONTACTS.send("granted"));
     }
 
     @Override
@@ -65,6 +81,47 @@ public class PermissionsActivity extends TestActivity {
     protected void onStop() {
         super.onStop();
         links.unlink();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.permissions, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        tell("CHECK: %s", item.getTitle());
+        switch (item.getItemId()) {
+            case R.id.request_coarse_location:
+                coarse.submit();
+                return true;
+            case R.id.request_fine_location:
+                fine.submit();
+                return true;
+            case R.id.request_read_contacts:
+                contacts.submit();
+                return true;
+            case R.id.request_combo:
+                AndroidPermit.of(this)
+                        .ask(Manifest.permission.ACCESS_COARSE_LOCATION,
+                                Manifest.permission.READ_CONTACTS)
+                        .granted(() -> {
+                            COARSE.send("granted");
+                            CONTACTS.send("granted");
+                        })
+                        .submit();
+                return true;
+            case R.id.reset_permissions:
+                try {
+                    Runtime.getRuntime().exec("pm clear ph.codeia.androidutilstests");
+                } catch (IOException e) {
+                    tell(e.toString());
+                }
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private Channel.Link connect(Channel<String> channel, String desc) {
@@ -83,99 +140,19 @@ public class PermissionsActivity extends TestActivity {
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.permissions, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        tell("CHECK: %s", item.getTitle());
-        switch (item.getItemId()) {
-            case R.id.request_coarse_location:
-                requests.get(0).submit();
-                return true;
-            case R.id.request_fine_location:
-                requests.get(1).submit();
-                return true;
-            case R.id.request_read_contacts:
-                requests.get(2).submit();
-                return true;
-            case R.id.request_combo:
-                requests.get(3).submit();
-                return true;
-            case R.id.reset_permissions:
-                try {
-                    Runtime.getRuntime().exec("pm clear ph.codeia.androidutilstests");
-                } catch (IOException e) {
-                    tell(e.toString());
-                }
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        for (Sensitive s : requests) {
-            if (s.check(requestCode, permissions, grantResults)) {
-                s.dispatch();
-                return;
-            }
-        }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
-
-    OldPermit ask(String... permissions) {
-        int code = counter.getAndIncrement();
-        return new AndroidOldPermit(this, code).ask(permissions).denied(appeal -> {
-            onDeny(appeal);
-            if (!appeal.isEmpty()) {
-                new AlertDialog.Builder(this)
-                        .setTitle("Please?")
-                        .setMessage(""
-                                + "I need these permissions to continue:\n-  "
-                                + TextUtils.join("\n-  ", appeal))
-                        .setPositiveButton("Ask me again", (dialog, id) -> appeal.submit())
-                        .create()
-                        .show();
-            } else {
-                tell("permanently denied:\n-  %s", TextUtils.join("\n-  ", appeal.banned()));
-            }
-        });
-    }
-
-    static void onDeny(Sensitive appeal) {
-        for (String s : appeal) switch (s) {
+    private static void publish(String message, Iterable<String> permissions) {
+        for (String p : permissions) switch (p) {
             case Manifest.permission.ACCESS_COARSE_LOCATION:
-                COARSE.send("appeal");
+                COARSE.send(message);
                 break;
             case Manifest.permission.ACCESS_FINE_LOCATION:
-                FINE.send("appeal");
+                FINE.send(message);
                 break;
             case Manifest.permission.READ_CONTACTS:
-                CONTACTS.send("appeal");
+                CONTACTS.send(message);
                 break;
             default:
-                EDGE_CASE.send("appeal " + s);
-                break;
-        }
-        for (String s : appeal.banned()) switch (s) {
-            case Manifest.permission.ACCESS_COARSE_LOCATION:
-                COARSE.send("denied");
-                break;
-            case Manifest.permission.ACCESS_FINE_LOCATION:
-                FINE.send("denied");
-                break;
-            case Manifest.permission.READ_CONTACTS:
-                CONTACTS.send("denied");
-                break;
-            default:
-                EDGE_CASE.send("denied " + s);
+                EDGE_CASE.send(message + " " + p);
                 break;
         }
     }
